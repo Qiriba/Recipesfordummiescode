63b9431382c5e127888c81a92cd902b2d5553d07
Qiriba <136338149+Qiriba@users.noreply.github.com>
2023-11-09 14:09:00 +0100

    Changed compile version to 34 o the App runs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.recipesfordummies"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.recipesfordummies"
