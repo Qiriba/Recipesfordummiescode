756e5132406b3a9fa8cda6e5584257d83786129e
Qiriba <136338149+Qiriba@users.noreply.github.com>
2024-04-30 14:02:11 +0200

    Updated gradle Build versions and changed compile sdk to version 34 and also Added Unit tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.rezeptefuerdummies"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.rezeptefuerdummies"
@@ -36,12 +36,14 @@ android {
 }
 
 dependencies {
-    implementation("com.simplecityapps:recyclerview-fastscroll:1.0.27")
     implementation("com.squareup.picasso:picasso:2.8")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.8.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.core:core-ktx:+")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.0.0")
+    implementation("androidx.core:core-ktx:+")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/test/java/com/example/rezeptefuerdummies/ExampleUnitTest.kt
+++ app/src/test/java/com/example/rezeptefuerdummies/ExampleUnitTest.kt
@@ -11,7 +11,22 @@ import org.junit.Assert.*
  */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun getItemId(){
+        val feedItemList: MutableList<FeedItemModel> = createDummyData()
+        val adapter = FeedAdapter(feedItemList)
+        assertEquals(adapter.getItemId(1), -693801673)
+        assertEquals(adapter.getItemId(0), 1117640830)
+    }
+
+    fun createDummyData(): MutableList<FeedItemModel> {
+        val dummyData = mutableListOf<FeedItemModel>()
+
+        // Add your feed items to the list
+        dummyData.add(FeedItemModel("https://karlsruhepuls.de/wp-content/uploads/2023/06/Thai-Food-Festival-Karlsruhe.jpg", "Thai Curry", "Hard", "45 Min", "Klassisch", 1))
+        dummyData.add(FeedItemModel("https://www.chefsculinar.de/chefsculinar/ds_img/assets_700/2014-09-04-Doener-690x460.jpg", "Döner Kebap", "Medium", "1 H", "Klassisch", 2))
+        dummyData.add(FeedItemModel("https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/b36fbe87cb4d6e6e3dce4b23aa35e481/Derivates/563a4efc4ab575cad5db7a9279096132b4334a7c.jpg", "Deutsche Rumpsteak", "Hard", "45 Min", "Klassisch", 3))
+        dummyData.add(FeedItemModel("https://www.lidl-kochen.de/images/recipe-wide/860844/veganer-gyrosteller-mit-reis-und-salat-311084.jpg", "Veganer Gyros Teller", "Easy", "30 Min", "Vegan", 4))
+        dummyData.add(FeedItemModel("https://img.chefkoch-cdn.de/rezepte/2529831396465550/bilder/1509532/crop-960x720/pfannkuchen-crepe-und-pancake.jpg", "Omas Pfannkuchen", "Easy", "20 Min", "Vegetarisch", 5))
+        return dummyData
     }
 }
