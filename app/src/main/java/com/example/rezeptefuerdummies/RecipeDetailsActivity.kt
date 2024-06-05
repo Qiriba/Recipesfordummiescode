63035f64f45062ae013569791b00f4d7672a0849
Qiriba <136338149+Qiriba@users.noreply.github.com>
2024-06-05 16:07:59 +0200

    Changing permission of gradlew

--- app/src/main/java/com/example/rezeptefuerdummies/RecipeDetailsActivity.kt
+++ app/src/main/java/com/example/rezeptefuerdummies/RecipeDetailsActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.rezeptefuerdummies.databinding.ActivityRecipeDetailsBinding
 
 class RecipeDetailsActivity : AppCompatActivity() {
 
