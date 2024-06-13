8339743541e1ba06042c3b3bfa06707fc4a21220
Qiriba <136338149+Qiriba@users.noreply.github.com>
2024-06-13 20:24:59 +0200

    Added Button support

--- app/src/main/java/com/example/rezeptefuerdummies/StartRecipeActivity.kt
+++ app/src/main/java/com/example/rezeptefuerdummies/StartRecipeActivity.kt
@@ -1,14 +1,29 @@
 package com.example.rezeptefuerdummies
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class StartRecipeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start_recipe)
-
+        val nextButton = findViewById<Button>(R.id.asr_nextButton)
+        nextButton.setOnClickListener {
+            nextButtonClick()
+        }
+        val prevButton = findViewById<Button>(R.id.asr_prevButton)
+        nextButton.setOnClickListener {
+            prevButtonClick()
+        }
         val recipeID = intent.getStringExtra("id")
+
+    }
+    fun nextButtonClick(){
+
+    }
+
+    fun prevButtonClick(){
 
     }
 }
