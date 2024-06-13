98aa368bfa8a61544413e3c8a8d3f10160b33f4d
Qiriba <136338149+Qiriba@users.noreply.github.com>
2024-06-13 20:45:18 +0200

    Improved SRA

--- app/src/main/java/com/example/rezeptefuerdummies/StartRecipeActivity.kt
+++ app/src/main/java/com/example/rezeptefuerdummies/StartRecipeActivity.kt
@@ -5,25 +5,30 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class StartRecipeActivity : AppCompatActivity() {
+    var currentStepIndex = 0
+    var stepArray = emptyArray<stepItem>()
+    val recipeID = intent.getStringExtra("id")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start_recipe)
+
         val nextButton = findViewById<Button>(R.id.asr_nextButton)
         nextButton.setOnClickListener {
             nextButtonClick()
         }
+
         val prevButton = findViewById<Button>(R.id.asr_prevButton)
         nextButton.setOnClickListener {
             prevButtonClick()
         }
-        val recipeID = intent.getStringExtra("id")
-
     }
-    fun nextButtonClick(){
 
+    fun nextButtonClick(){
+        currentStepIndex++
     }
 
     fun prevButtonClick(){
-
+        currentStepIndex--
     }
+
 }
